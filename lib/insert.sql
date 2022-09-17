1c2644613f9033f596a7aeaff8ec14f8ea3c832f
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-09-16 17:38:01 -0700

    commit tree 9c96c21b3074d371a37f8efc4d2e0e14673d2cc8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 5, "M", "brown", "sharp", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 8, "M", "white", "beligerent", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 55, "F", "purple", "sleepy", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 500, "M", "brown", "militarized", 1);
+INSERT INTO bears VALUES (5,"Melissa", 111, "F", "black", "cool", 1);
+INSERT INTO bears VALUES (6,"Grinch", 12, "M", "green", "grump", 1);
+INSERT INTO bears VALUES (7,"Wendy", 0, "F", "orange", "cute", 1);
+INSERT INTO bears VALUES (8,null, 50, "M", "none", "cool", 0);
